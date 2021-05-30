e694cc310fdbc6d776e396ccb2ccb190cdcc4c18
pritam634 <83156982+pritam634@users.noreply.github.com>
2021-05-30 17:25:36 +0530

    dev portfolio

---
+++ js/font-type.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.type', {
+    strings: [
+        'Student of Computer Science',
+        'Social Media influencer',
+        'Social Media Entrepreneur'
+        // 'Founder at Shiksha Ghar',
+        // 'CEO at Shiksha Ghar',
+        // 'Leader, Self Learner'
+    ],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+});
